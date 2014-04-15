fbaca025927c26796f480c0cf6c4cb8060d1eaf8
Cahya Ong <cahya.ong@gmail.com>
2014-04-15 21:29:45 +1000

    Moved accent color selection into application start-up event.

--- Cop.Theia.Client.Wpf/App.xaml.cs
+++ Cop.Theia.Client.Wpf/App.xaml.cs
@@ -1,6 +1,17 @@
 ﻿namespace Cop.Theia.Client.Wpf
 {
+    using System.Windows;
+    using System.Windows.Media;
+
+    using FirstFloor.ModernUI.Presentation;
+
     public partial class App
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            AppearanceManager.Current.AccentColor = Color.FromRgb(0x53, 0x81, 0x35);
+        }
     }
 }
--- Cop.Theia.Client.Wpf/AppBootstrapper.cs
+++ Cop.Theia.Client.Wpf/AppBootstrapper.cs
@@ -1,16 +1,8 @@
 ﻿namespace Cop.Theia.Client.Wpf
 {
-    using System.Windows.Media;
-
     using Caliburn.Micro;
-
-    using FirstFloor.ModernUI.Presentation;
 
     internal class AppBootstrapper : Bootstrapper<AppViewModel>
     {
-        public AppBootstrapper()
-        {
-            AppearanceManager.Current.AccentColor = Color.FromRgb(0x53, 0x81, 0x35);
-        }
     }
 }
