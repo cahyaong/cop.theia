c83623f4187083d97ce8392a517a2791cb07aa2e
Cahya Ong <cahya.ong@gmail.com>
2014-04-15 21:08:34 +1000

    Changed the default accent color of the main application.

--- Cop.Theia.Client.Wpf/AppBootstrapper.cs
+++ Cop.Theia.Client.Wpf/AppBootstrapper.cs
@@ -1,8 +1,16 @@
 ﻿namespace Cop.Theia.Client.Wpf
 {
+    using System.Windows.Media;
+
     using Caliburn.Micro;
+
+    using FirstFloor.ModernUI.Presentation;
 
     internal class AppBootstrapper : Bootstrapper<AppViewModel>
     {
+        public AppBootstrapper()
+        {
+            AppearanceManager.Current.AccentColor = Color.FromRgb(0x53, 0x81, 0x35);
+        }
     }
 }
