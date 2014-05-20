7a5bbf3188cbd18210c56cad0100851c5b026f14
Cahya Ong <cahya.ong@gmail.com>
2014-05-21 00:25:12 +1000

    Added XML namespace definition for core libraries.

--- nGratis.Cop.Core.Wpf/Properties/AssemblyInfo.cs
+++ nGratis.Cop.Core.Wpf/Properties/AssemblyInfo.cs
@@ -1,9 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 [assembly: AssemblyTitle("nGratis.Cop.Core.Wpf")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("http://schemas.ngratis.com/2014/core", "nGratis.Cop.Core.Wpf")]
--- nGratis.Cop.Core/Properties/AssemblyInfo.cs
+++ nGratis.Cop.Core/Properties/AssemblyInfo.cs
@@ -1,8 +1,10 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows.Markup;
 
 [assembly: AssemblyTitle("nGratis.Cop.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("aad5b1cf-5af1-4011-a396-493e640ab914")]
+[assembly: XmlnsDefinition("http://schemas.ngratis.com/2014/core", "nGratis.Cop.Core")]
