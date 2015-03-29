b8870679b518add3cd6799be7d55ec6d9d7cecba
Cahya Ong <cahya.ong@gmail.com>
2015-03-29 15:56:17 +1100

    Added a new sandbox project for my Kaggle competition.

---
+++ nGratis.Cop.Theia.Module.Application/ApplicationModule.cs
@@ -0,0 +1,54 @@
+﻿// ------------------------------------------------------------------------------------------------------------------------------------------------------------
+// <copyright file="ApplicationModule.cs" company="nGratis">
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2014 - 2015 Cahya Ong
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+// </copyright>
+// <author>Cahya Ong - cahya.ong@gmail.com</author>
+// <creation_timestamp>Sunday, 29 March 2015 4:26:34 AM</creation_timestamp>
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace nGratis.Cop.Theia.Module.Application
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+
+    using nGratis.Cop.Core.Contract;
+
+    [Export(typeof(IModule))]
+    public class ApplicationModule : IModule
+    {
+        public ApplicationModule()
+        {
+            this.Id = new Guid("7B16E195-F198-42F8-B49D-C14A6594EBB1");
+
+            var diabeticRetinopathyPage = new Page("Kaggle", @"/nGratis.Cop.Theia.Module.Application;component/Kaggle/DiabeticRetinopathyView.xaml");
+            var applicationFeature = new Feature("Application", new List<Page> { diabeticRetinopathyPage });
+
+            this.Features = new List<Feature> { applicationFeature };
+        }
+
+        public Guid Id { get; private set; }
+
+        public IEnumerable<Feature> Features { get; private set; }
+    }
+}
