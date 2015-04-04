6754f5a0e6dd1e2377b5a5ae13398ec898303ea8
Cahya Ong <cahya.ong@gmail.com>
2015-04-05 07:53:34 +1000

    Renamed a variable in the pre-build script.

--- Source/nGratis.Cop.Core/Properties/nGratis.Cop.Core.PreBuild.ps1
+++ Source/nGratis.Cop.Core/Properties/nGratis.Cop.Core.PreBuild.ps1
@@ -9,16 +9,16 @@ $CurrentFolderPath = "$([System.IO.Path]::GetDirectoryName($MyInvocation.MyComma
 
 if ($Configuration -eq "Release")
 {
-	$T4ExeFilePath = [System.Environment]::ExpandEnvironmentVariables("%CommonProgramFiles(x86)%\Microsoft Shared\TextTemplating\12.0\TextTransform.exe")
+	$T4ExePath = [System.Environment]::ExpandEnvironmentVariables("%CommonProgramFiles(x86)%\Microsoft Shared\TextTemplating\12.0\TextTransform.exe")
 
-	if (-not (Test-Path $T4ExeFilePath))
+	if (-not (Test-Path $T4ExePath))
 	{
 		throw "T4 Transform executable file is not found"
 	}
 
 	Write-Host "**   Transforming global assembly info template..."
 	$Version = "0.1.0.0"
-	& $T4ExeFilePath "$($CurrentFolderPath)..\..\nGratis.Cop.Theia.Common\GlobalAssemblyInfo.tt" -a !!Version!$Version
+	& $T4ExePath "$($CurrentFolderPath)..\..\nGratis.Cop.Theia.Common\GlobalAssemblyInfo.tt" -a !!Version!$Version
 }
 
 Write-Host "** Finishing nGratis.Cop.Core PRE-BUILD script..."
